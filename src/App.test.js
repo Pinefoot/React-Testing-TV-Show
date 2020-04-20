6a49fe84a0f9243283eb889f511c989fe061ee88
Pinefoot <jameslcarpino@gmail.com>
2020-04-20 13:34:15 -0700

    initialized and set up project. moving on.

---
+++ src/App.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render, fireEvent, waitFor} from '@testing-library/react';
+//import MissionsList from './MissionsList';
+import { as mock} from './api/'
+import App from './App';
